cb05171d82ab5a91c7c53672e5ce59d03a9f9fcf
moequan <maurice.s93@web.de>
2019-09-17 10:47:04 +0200

    Including first,last and particular remove from elements ( shopping cart).

--- shopping/shop.js
+++ shopping/shop.js
@@ -3,8 +3,64 @@
 //Session Storage: It is same like local storage date except it will delete all data when browser windows closed by a web user.
 //set the value in local storage object
 //localStorage.setItem("dci","Web Developer");
-console.log(localStorage.getItem("dci"));
-//localStorage.removeItem("dci");
-//localStorage.clear();
-sessionStorage.setItem("classroom","FbW15");
-console.log(sessionStorage.getItem("classroom"));
+// console.log(localStorage.getItem("dci"));
+// //localStorage.removeItem("dci");
+// //localStorage.clear();
+// sessionStorage.setItem("classroom","FbW15");
+// console.log(sessionStorage.getItem("classroom"));
+
+let addGiftObj = document.getElementById("addGift");
+
+addGiftObj.onclick = () => {
+    console.log(`Am I work ? ..`)
+
+    let giftObj = document.getElementById("gift");
+    let shoppingObj = document.getElementById("shoppingList");
+    let newGiftObj = document.createElement("li")
+    newGiftObj.innerHTML = giftObj.value;
+    shoppingObj.appendChild(newGiftObj)
+}
+
+
+let removeLastGiftObj = document.getElementById("removeLastGift");
+
+removeLastGiftObj.onclick = () => {
+
+    let allGiftObj = document.querySelectorAll("#shoppingList > li");
+    allGiftObj[allGiftObj.length - 1].remove()
+}
+
+
+let removeFirstGiftObj = document.getElementById("removeFirstGift")
+
+removeFirstGiftObj.onclick = () => {
+
+    let firstGift = document.querySelectorAll("#shoppingList > li");
+    firstGift[0].remove()
+}
+
+let removeObj = document.getElementById("remove")
+
+removeObj.onclick = () => {
+
+    let allGiftObj = document.querySelectorAll("#shoppingList > li");
+    let removeGiftObj = document.getElementById("gift")
+
+
+    let found = false;
+
+    for (i = 0; i < allGiftObj.length; i++) {
+
+        let deleteGift = allGiftObj[i].innerHTML;
+
+        if (removeGiftObj.value.toLowerCase() === deleteGift.toLowerCase()) {
+            
+            allGiftObj[i].remove();
+            removeGiftObj = "";
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+        alert(`Gift ${removeGiftObj.value} not found...`)
+}
